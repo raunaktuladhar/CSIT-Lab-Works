0b88ed00f330bdd0732910b58a2d9931ce1aba32
Raunak Tuladhar <52153091+raunaktuladhar@users.noreply.github.com>
2025-02-14 14:25:01 +0545

    Delete 6th Semester/Net Centric Computing (C#)/Practical3 directory

--- 6th Semester/Net Centric Computing (C#)/Practical3/Qno2/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Qno2.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Department { get; set; }
-    }
-}
--- 6th Semester/Net Centric Computing (C#)/Practical3/Qno3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Qno3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
