1bb228a1306b958673a7dbbe6972399a2e6145f2
Raunak Tuladhar <52153091+raunaktuladhar@users.noreply.github.com>
2025-02-14 13:13:16 +0545

    Add files via upload

---
+++ 6th Semester/Net Centric Computing(C#)/Practical1/qno8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical1
+{
+    class Box
+    {
+        int length;
+        int breadth;
+        int height;
+
+        public Box(int length, int breadth, int height)
+        {
+            this.length = length;
+            this.breadth = breadth;
+            this.height = height;
+        }
+
+        public void getVol()
+        {
+            Console.WriteLine("The Volume of box is " + (length * breadth * height));
+        }
+    }
+
+    class SmallBox : Box
+    {
+        int weight;
+
+        public SmallBox(int length, int breadth, int height, int weight) : base(length, breadth, height)
+        {
+            this.weight = weight;
+        }
+
+        public int getWeight()
+        {
+            return weight;
+        }
+    }
+}
