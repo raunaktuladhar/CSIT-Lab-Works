1460d149241b3ad8b7e5e5c7dfefcf44f2cdb31c
Raunak Tuladhar <52153091+raunaktuladhar@users.noreply.github.com>
2025-02-14 14:34:25 +0545

    Add files via upload

---
+++ 6th Semester/Net Centric Computing (C#)/Practical3/Qno1/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qno1.Models;
+using System.Collections.Generic;
+
+namespace Qno1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Raunak", Age = 21, Gender = "Male", Department = "CSIT" },
+                new Student { Id = 2, Name = "Bigyan", Age = 20, Gender = "Male", Department = "BCA" },
+                new Student { Id = 3, Name = "Swosti", Age = 19, Gender = "Male", Department = "BIT" }
+            };
+
+            return View(students);
+        }
+    }
+}
