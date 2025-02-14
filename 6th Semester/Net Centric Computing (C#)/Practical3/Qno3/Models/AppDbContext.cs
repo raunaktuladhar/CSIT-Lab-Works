de8f027f102454fa5d4dc3b9bba6eafe052dc95a
Raunak Tuladhar <52153091+raunaktuladhar@users.noreply.github.com>
2025-02-14 14:40:19 +0545

    Add files via upload

---
+++ 6th Semester/Net Centric Computing (C#)/Practical3/Qno3/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Qno3.Models;
+
+namespace Qno3.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Officer> Officers { get; set; }
+    }
+}
---
+++ 6th Semester/Net Centric Computing (C#)/Practical3/Qno3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qno3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
